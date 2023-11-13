cda3d5fc053da84e89f28523e86d925e0dadf9f4
Harry <harry.mian@scoutsecured.com>
2023-11-13 13:18:04 -0500

    create user

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 const express = require("express");
-const userData = require("./mock_data/index");
+// const userData = require("./mock_data/index");
 const app = express();
+
+const users = [];
 
 app.get("/api/users", (req, res) => {
   const { skip = 0, limit = 10 } = req.query;
-
-  const slicedUsers = userData.users;
-
+  //   const slicedUsers = userData.users;
   res.json({
-    users: slicedUsers,
-    total: userData.total,
+    users: users,
+    total: users.total,
     skip: parseInt(skip),
     limit: parseInt(limit),
   });
+});
+
+app.post("/api/users", (req, res) => {
+  const newUser = req.body;
+  users.push(newUser);
+  res.json(newUser);
 });
 
 const server = app.listen(3001, () => {
