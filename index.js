b909c4c8e5cc6823bfb540e5167aca117e5c2cd9
Harry <harry.mian@scoutsecured.com>
2023-11-13 13:20:54 -0500

    changes

--- index.js
+++ index.js
@@ -15,10 +15,27 @@ app.get("/api/users", (req, res) => {
   });
 });
 
-app.post("/api/users", (req, res) => {
-  const newUser = req.body;
-  users.push(newUser);
-  res.json(newUser);
+// app.post("/api/users", (req, res) => {
+//   const newUser = req.body;
+//   users.push(newUser);
+//   res.json(newUser);
+// });
+
+app.post("/api/users", async (req, res) => {
+  try {
+    const { username, email } = req.body;
+
+    if (!username || !email) {
+      return res.status(400).json({ error: "Username and email are required" });
+    }
+
+    const newUser = users.push({ username, email });
+
+    res.status(201).json(newUser);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
 const server = app.listen(3001, () => {
